5b6d2b49529dd8283a7d22abef2c8f1a5dd6cc7e
Mwenya Guardian <mwenyaguardianmg@gmail.com>
2024-01-10 13:08:52 +0000

    Removed the initial way of user input and added express to get user data input

--- hello.js
+++ hello.js
@@ -1,60 +1,60 @@
 console.log("running!!!");
 const { process_params } = require('express/lib/router');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Sample data
+let books = [
+  {id: 1, title: "Book 1", author:"Author 1"},
+  {id: 1, title: "Book 1", author:"Author 1"}
+]
+let me = {
+  id:{is: "This"},
+  them: {you:"that"}
+}
+
+app.use(express.json());
+app.get('/books', (req, res)=> {
+  res.json(books.Me = me);
+});
+app.post('/books', (req, res)=> {
+  const newBook = req.body;
+  books.push(newBook);
+  res.status(201).json(newBook);
+});
+app.put('/books/:id', (req, res) => {
+  const bookId =  parseInt(req.params.id);
+  const updateBook = req.body;
+  books = books.map(book=> book.id !== bookId? updateBook: book);
+    res.json(updateBook);
+});
+app.delete('/books/:id', (req, res)=> {
+  const bookId = parseInt(req.params.id);
+  books = books.filter(book => book.id !== bookId);
+  res.sendStatus(204);
+});
+app.listen(port, ()=> {
+  console.log('REST API for book management is listening at http://localhost:${port}');
+});
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------
+//Previuos function definations
+//---------------------------------------------------------------------------------
 const readline =  require('readline');
 const req = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-var target, values;
-//Getting the user inpuit for the vcalues to be sorted and/or searched
-req.question("Enter the Values: ", (userInput) => {
-  values = userInput;
-  target = values.split(" ")[values.split(" ").length - 1];
-    console.log(values);
-    //req.close;
-    console.log(BinarySearch(values, target));
-    
-});
-//The number to be searched for
-req.question("Enter the target value: ", (userInput) => {
-  target = userInput;
-    console.log(target);
-    req.close;
-});
-function getValues(){
-  //event.preventDefault();
-  //var values = //document.getElementById("values").value;
-  //var target = ;//document.getElementById("target").value;
-  //document.write(values);
-  //document.getElementById("result").innerHTML = values.length;
-  //document.getElementById("resultSearch").innerHTML = target;
-  if(values.length == 0){
-    //document.getElementById("result").innerHTML = "Please enter the numbers you wish to sort and/or search";
-    console.log("Please enter the numbers you wish to sort and/or search");
-    return;
-  }
-    if("string" === typeof values)
-      values = values.split(" ");
-  if(values.length > 0 && target.length == 0){
-    toNumber(values);
-    sortAlg(values);
-      //document.getElementById("result").innerHTML = values;
-      console.log(values);
-      return;
-  } else if(values.length > 0 && target.length > 0){
-    target =  target.slpit(" ")[0];
-    target = parseInt(target);
-        if(isNaN(target)){
-          //document.getElementById("resultSearch").innerHTML = "Please enter a number for the search digit";
-          console.log("Please enter a number for the search digit");
-          return;
-        }
-        toNumber(values);
-        //document.getElementById("resultSearch").innerHTML = "Index Of Result: " + BinarySearch(values, target);
-        console.log("Index Of Result: " + BinarySearch(values, target));
-        return;
-  }
-}
 
 function sortAlg(arr){
   if(!Array.isArray(arr))
